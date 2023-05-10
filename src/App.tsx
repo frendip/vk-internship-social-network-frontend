4a604d4353fc336fa93fcf18957bbfda32c2e2b3
Pavel Pavlov <ppavvvlov@gmail.com>
2023-05-10 05:37:06 +0300

    feature (Header) разработан header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import './styles/styles.module.scss';
+import Registration from './pages/Registration/Registration';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<div>main</div>} />
+          <Route path="/login" element={<div>login</div>} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="*" element={<div>not found</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Registration/Registration.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Registration.module.scss';
+
+const Registration = () => {
+  return (
+    <div className={classes.registration}>
+      <div className={classes.registration__card}>1213</div>
+    </div>
+  );
+};
+
+export default Registration;
